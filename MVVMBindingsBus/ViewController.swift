d7cf27c4427289d0a0d4a7981f524f95d041f4ad
Samir Mansour <mansour.69@osu.edu>
2022-03-04 17:11:13 -0500

    Initial Commit

---
+++ MVVMBindingsBus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMBindingsBus
+//
+//  Created by Samir Mansour on 3/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
